8ebd21d439e654a6e155cc126a1cb9842afced9f
EliasSchlie <elias.fake31@gmail.com>
2024-12-19 11:06:22 +0100

    Week3 License plate checker task

---
+++ Week3/plates/plates.py
@@ -0,0 +1,27 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if 2 > len(s) or len(s) > 6:
+        return False
+    elif not s[0].isalpha() or not s[1].isalpha():
+        return False
+    condition = True
+    for letter in s:
+        if condition and letter.isalpha():
+            condition = True
+        elif condition and letter == "0":
+            return False
+        elif letter.isdigit():
+            condition = False
+        else:
+            return False
+    return True
+
+
+main()
