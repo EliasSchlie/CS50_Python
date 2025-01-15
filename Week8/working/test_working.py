b8c6aff62c6a0ad60ff1c7f43d4916c804185e2b
EliasSchlie <elias.fake31@gmail.com>
2025-01-15 15:34:27 +0100

    Added a third testing function for unit test

--- Week8/working/test_working.py
+++ Week8/working/test_working.py
@@ -15,3 +15,8 @@ def test_error():
         convert("9:00 AM - 17:00 PM")
     with pytest.raises(ValueError):
         convert("9:60 AM to 8:60 PM")
+
+def test_3():
+    assert convert("9:01 AM to 5:01 PM") == "09:01 to 17:01"
+    assert convert("9:00 PM to 5:00 AM") == "21:00 to 05:00"
+    assert convert("10 PM to 8 AM") == "22:00 to 08:00"
